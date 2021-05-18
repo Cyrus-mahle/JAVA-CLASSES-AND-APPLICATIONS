673638a028c626f5d8b4df1395e891f1ff2e63ea
Cyrus-mahle <67721071+Cyrus-mahle@users.noreply.github.com>
2021-05-18 02:19:44 +0200

    Add files via upload

---
+++ ProgramApp.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane; // import library
+
+public class ProgramApp {          // start class
+
+	public static void main(String[] args) {  // start main method 
+	
+     JOptionPane.showMessageDialog(null, "Programming is Fun!"); // prompt the user
+     
+     System.exit(0);  
+     
+	}
+
+}
